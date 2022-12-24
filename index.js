1f5800f4da401f717735683c89158673bba2f86b
HerokuNivas <heroku.nivas.deployments@gmail.com>
2022-12-24 10:40:45 +0530

    Rejected

--- index.js
+++ index.js
@@ -110,6 +110,9 @@ async function dataUpdateGame(USERNAME, PASSWORD, REQUEST){
         const databaseIs = client.db("AsyncTicTacToe");
         const collectionIs = databaseIs.collection("games");
         const res = await collectionIs.updateOne({user1: REQUEST.user1, user2: REQUEST.user2}, {$set:{user1: REQUEST.user1, user2: REQUEST.user2, current: REQUEST.current, board: REQUEST.board, winby: REQUEST.winby, time: REQUEST.time}});
+        if(REQUEST.winby !== ""){
+            dataRejectGame(USERNAME, PASSWORD, REQUEST.user1, REQUEST.user);
+        }
     }
     catch(err) {
         return "Done";
